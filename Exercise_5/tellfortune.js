7fa929ad58a65927fa924b472db4b277775d5868
monicajerald <monicajerald@gmail.com>
2023-04-13 13:47:35 -0400

    Fortune Teller

---
+++ Exercise_5/tellfortune.js
@@ -0,0 +1,7 @@
+function tellfortune(NoofChildren,Partnersname,Geoloc,Jobtitle)
+{console.log
+    ("You will be a "+Jobtitle+" in "+Geoloc+" and married to "+Partnersname+" with "+NoofChildren+" kids")
+}
+tellfortune(2,"David","India","Attorney")
+tellfortune(3,"Solomon","London","Doctor")
+tellfortune(4,"Immanuel","USA","Software Engineer")
