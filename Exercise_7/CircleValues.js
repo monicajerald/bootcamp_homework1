193ffd285b20eea6cd49846b37e902fecf9bf253
monicajerald <monicajerald@gmail.com>
2023-04-14 11:00:41 -0400

    Circle Values

---
+++ Exercise_7/CircleValues.js
@@ -0,0 +1,11 @@
+function CircleValues(radius)
+{
+    var pi=3.14
+    var area=(pi*(radius*radius))
+    console.log(("The area of this circle is "+area+""))
+    var peri=(2*pi*radius)
+    console.log("The perimeter of a circle is "+peri+"")
+}
+CircleValues(5)
+CircleValues(10)
+CircleValues(15)
