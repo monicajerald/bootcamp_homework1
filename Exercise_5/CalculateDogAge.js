b791c8e4c8e2743e126098de9c149bfba7dfeaee
monicajerald <monicajerald@gmail.com>
2023-04-13 21:29:45 -0400

    Calculate Dog Age

---
+++ Exercise_5/CalculateDogAge.js
@@ -0,0 +1,8 @@
+function CalculateDogAge(DogsAge)
+{
+    var dogyears = 7 * DogsAge;
+    console.log("Your doggy is "+dogyears+" years old in dog years!")
+}
+CalculateDogAge(1)
+CalculateDogAge(2)
+CalculateDogAge(3)
