6f050f27226eefcaea9a44ee490c127551f15624
monicajerald <monicajerald@gmail.com>
2023-04-13 22:10:49 -0400

    Calculate Getmetric

---
+++ Exercise_5/CalcCircumferArea.js
@@ -0,0 +1,11 @@
+function CalcCircumferArea(radius)
+{
+    var pi=3.14
+    var Circumference=(2*pi*radius)
+    console.log("The Circumference of a Circle is "+Circumference+"")
+    var area=(pi*(radius*radius))
+    console.log("The area of a Circle is "+area+"")
+}
+CalcCircumferArea(5)
+CalcCircumferArea(10)
+CalcCircumferArea(15)
